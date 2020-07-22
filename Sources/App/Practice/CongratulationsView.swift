5bdcc7ef2b8bd6d7d19ca9652ecac64b27d27a95
zijievv <zijie_vv@icloud.com>
2020-07-23 01:20:22 +0800

    Refactor CongratulationsView

--- Sources/App/Practice/CongratulationsView.swift
+++ Sources/App/Practice/CongratulationsView.swift
@@ -49,39 +49,9 @@ struct CongratulationsView: View {
         .foregroundColor(.gray)
       
       ZStack {
-        VStack(spacing: 0) {
-          Rectangle()
-            .frame(height: 90)
-            .foregroundColor(
-              Color(red: 0.5, green: 0, blue: 0)
-                .opacity(0.2)
-            )
-          
-          Rectangle()
-            .frame(height: 90)
-            .foregroundColor(
-              Color(red: 0.6, green: 0.1, blue: 0.1)
-                .opacity(0.4)
-            )
-        }
-        
-        Image(systemName: "person.fill")
-          .resizable()
-          .padding()
-          .frame(width: avatarSize, height: avatarSize)
-          .background(Color.white.opacity(0.5))
-          .cornerRadius(avatarSize / 2, antialiased: true)
-          .shadow(radius: 4)
-        
-        VStack {
-          Spacer()
-          Text(userName)
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .fontWeight(.bold)
-            .shadow(radius: 7)
-        }
-        .padding()
+        avatarBackgroundView
+        avatarView
+        avatarUserNameView
       }
       .frame(height: 180)
       
@@ -105,5 +75,47 @@ struct CongratulationsView_Previews: PreviewProvider {
   static var previews: some View {
     CongratulationsView(userName: "Johnny Swift")
       .environmentObject(ChallengesViewModel())
+  }
+}
+
+extension CongratulationsView {
+  var avatarView: some View {
+    Image(systemName: "person.fill")
+      .resizable()
+      .padding()
+      .frame(width: avatarSize, height: avatarSize)
+      .background(Color.white.opacity(0.5))
+      .cornerRadius(avatarSize / 2, antialiased: true)
+      .shadow(radius: 4)
+  }
+  
+  var avatarBackgroundView: some View {
+    VStack(spacing: 0) {
+      Rectangle()
+        .frame(height: 90)
+        .foregroundColor(
+          Color(red: 0.5, green: 0, blue: 0)
+            .opacity(0.2)
+        )
+      
+      Rectangle()
+        .frame(height: 90)
+        .foregroundColor(
+          Color(red: 0.6, green: 0.1, blue: 0.1)
+            .opacity(0.4)
+        )
+    }
+  }
+  
+  var avatarUserNameView: some View {
+    VStack {
+      Spacer()
+      Text(userName)
+        .font(.largeTitle)
+        .foregroundColor(.white)
+        .fontWeight(.bold)
+        .shadow(radius: 7)
+    }
+    .padding()
   }
 }
